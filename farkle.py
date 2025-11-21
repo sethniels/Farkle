fdfef6e236f328ba9dfdc6b0316363a8e75a4615
Seth Nielsen <sethniels@geemail.com>
2025-11-21 15:46:15 -0700

    one die, first order model

--- farkle.py
+++ farkle.py
@@ -20,11 +20,23 @@ def dice_combo_generator (numdice : int):
                 curr_combo[i] = 1
     yield curr_combo
 
+def get_points (dice):
+    total = 100.0*dice.count(1) + 50.0*dice.count(5)
+    print(f"{dice=}; {total=}")
+    return total
 
 def farkle (numdice : int):
+    scoring_rolls = []
     for dice_combo in dice_combo_generator(numdice):
-        print(dice_combo)
+        points = get_points(dice_combo)
+        print(f"{points=}; {scoring_rolls=}")
+        if points:
+            scoring_rolls.append(points)
 
+    num_farkles = 6**numdice - len(scoring_rolls)
+    break_even_score = sum(scoring_rolls) / num_farkles
+    print(f"Current score must be less than or equal to {break_even_score} before rolling {numdice} dice")
+    print(f"{num_farkles=}; ")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculates the probability "
